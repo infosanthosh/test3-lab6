858cb0cc7e11c19ec217ce7b2782c6f501680c0e
infosanthosh <125239154+infosanthosh@users.noreply.github.com>
2023-02-13 21:51:28 +0530

    Update Demo1.java

--- Demo1.java
+++ Demo1.java
@@ -1,5 +1,5 @@
 public class Demo1{
                 public static void main(String[] args){
-                    System.out.println("Hello World ");
+                    System.out.println("Hello World This is my first jenkins demo : %date% : %time% ");
                 }
             }
